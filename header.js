bceaf6e38978ba083c316aa225b4014e67bc8eef
hamzah79 <hamzah.pujiningrat@gmail.com>
2025-03-19 21:35:06 +0700

    Delete header.js

--- header.js
+++
@@ -1,9 +0,0 @@
-	atOptions = {
-		'key' : '50ca6946bb1cf36dd3cfffa73357a266',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-document.write('<scr' + 'ipt type="text/javascript"
-<script type="text/javascript" src="//disembodyflick.com/50ca6946bb1cf36dd3cfffa73357a266/invoke.js"></scr' + 'ipt>');
